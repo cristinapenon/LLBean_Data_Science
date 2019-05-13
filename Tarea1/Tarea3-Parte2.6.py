86fdf5c94f910988480f4f64f75bcdf8cdf609ce
cristinapenon <portmann90>
2019-05-13 16:45:05 -0600

    Ejercicios para session 2

---
+++ Tarea1/Tarea3-Parte2.6.py
@@ -0,0 +1,21 @@
+cantidad=input("Dígame cuántas palabras tiene la lista:")
+cantidad=int(cantidad)
+nombres=[]
+if cantidad == 0:
+    print("Imposible!")
+else:
+    for x in range(cantidad):
+        nom=input("Dígame la palabra  :")
+        nombres.append(nom)
+
+list=[]
+for x in range(cantidad):
+   # print(nombres[x])
+    list.append(nombres[x])
+
+print("Esta es la lista original:",list)
+
+#sort = sorted(list, key=lambda tup: tup[3], reverse=True)
+sort = sorted(list, key=lambda tup: tup[1], reverse=False)
+
+print("Esta es la lista inversa:",sort)
