237e543be8c560a85fa117e34629f6892b29a7b8
cristinapenon <portmann90>
2019-05-12 21:14:36 -0600

    Ejercicios para session 2

---
+++ Tarea1/Tarea3-Parte2.2.py
@@ -0,0 +1,21 @@
+cantidad=input("Dígame cuántas palabras tiene la lista:")
+cantidad=int(cantidad)
+nombres=[]
+if cantidad == 0:
+    print("Imposible!")
+else:
+    for x in range(cantidad):
+        nom=input("Dígame la palabra :")
+        nombres.append(nom)
+
+for x in range(cantidad):
+    print(nombres[x])
+
+duda = input("Dígame la palabra a buscar :")
+
+matching = [s for s in nombres if duda in s]
+if len(matching)==0:
+    print("la palabra", duda," no existe en la lista")
+else:
+    y=len(matching)
+    print("La palabra",duda," aparece", y," vez/veces en la lista.")
---
+++ Tarea1/Tarea3-Parte2.py
@@ -0,0 +1,26 @@
+#PARTE!
+#Funcionamiento sugerido
+
+#Dígame cuántas palabras tiene la lista: 3
+#Dígame la palabra 1: Alberto
+#Dígame la palabra 2: Benito
+#Dígame la palabra 3: Carmen
+#La lista creada es: ['Alberto', 'Benito', 'Carmen']
+#Dígame cuántas palabras tiene la lista: 0
+#¡Imposible!
+
+
+cantidad=input("Dígame cuántas palabras tiene la lista:")
+cantidad=int(cantidad)
+nombres=[]
+if cantidad == 0:
+    print("Imposible!")
+else:
+    for x in range(cantidad):
+        nom=input("Dígame la palabra :")
+        nombres.append(nom)
+
+for x in range(cantidad):
+    print(nombres[x])
+
+
