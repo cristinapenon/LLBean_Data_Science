51bac231ccae2d26b9af05eaaaaecd75eec53160
cristinapenon <portmann90>
2019-05-14 21:00:13 -0600

    Tarea 1-Cenfotec

---
+++ Session3/ejemplo_funciones.py
@@ -0,0 +1,10 @@
+#ejemplo de funciones
+#crear una funcion que me permita sumar los umeros dados
+
+def sumar(*i,**n):
+    pass #no puedo tener un bloque vacio---el pass nos da chance de seguir sin errores de sintaxis
+
+resultado=sumar(4,5,nombre='felix', appellido='arias',numero=10)
+#resultado=sumar(5,7,8,9,1,8,'x')
+# los que tiene nombre del parametro, osea los nombre=, appeliido= son los que van al diccionario.
+#si los tiene que mezclar tiene que poner tupla al inicio y diccionario al final
---
+++ Session3/ejemplo_listas.py
@@ -0,0 +1,10 @@
+#operaciones con listas
+mi_lista=[
+    (1,2,3,4),
+    (5,6,7,8),
+    (9,10,11,12),
+]
+
+pass #el punto rojo permite
+#pass, pare para ver variables y luego siga. es un placeholder. no se necesita para un placeholder.
+#es una manera de ponerlo de manera segura
