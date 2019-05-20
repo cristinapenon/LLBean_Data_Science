2b7f190150a1744756ab14fc2935d70bde33e2e5
cristinapenon <portmann90>
2019-05-20 16:45:45 -0600

    Tarea 1-Cenfotec

--- Tarea2/Tarea1-Agenda.py
+++ Tarea2/Tarea1-Agenda.py
@@ -32,53 +32,66 @@ dictpact1={'identificación':114380418,'Nombre':'Cristina','Apellido':'Penon','T
 dictpact2={'identificación':112320312,'Nombre':'Monica','Apellido':'Penon','Tel':7896,'Dirección':'San Francsico','listaenfermedades':['dolor de estomago'], 'listamed':['jarabe']}
 dictpact3={'identificación':113051813,'Nombre':'Margarita','Apellido':'Baudrit','Tel':8526,'Dirección':'San Jose','listaenfermedades':['diabetes','migrañas'], 'listamed':['insulina','botox']}
 
-print(dictpact1)
-print(dictpact2)
-print(dictpact3)
+print("Información de paciente 1:",dictpact1)
+print("Información de paciente 2:",dictpact2)
+print("Información de paciente 3:",dictpact3)
 
 pacientes = {114380418: dictpact1, 112320312: dictpact2, 113051813:dictpact3}
 
-print(pacientes)
+print("Este es el dicionario de todos los pacientes:",pacientes)
 
 #ingreso de un paciente nuevo
 pacientes[214580418] = {'identificación':214580418,'Nombre':'Andres','Apellido':'Hernandez','Tel':1874,'Dirección':'Santa Ana','listaenfermedades':['dolor de estomago','dolor muscular'], 'listamed':['voltaren','jarabe']}
 
-print(pacientes)
+print("Este es el nuevo diccionario agregando 1 paciente mas:",pacientes)
 
 #borrar 1 paciente
 del pacientes[114380418]
-print(pacientes)
+print("Este es el nuevo diccionaio borranco 1 paciente:",pacientes)
 
 #Agregar más enfermedades en un paciente en particular
-dictpact4={'identificación':113051813,'Nombre':'Margarita','Apellido':'Baudrit','Tel':8526,'Dirección':'San Jose','listaenfermedades':['diabetes','migrañas','embarazo'], 'listamed':['insulina','botox']}
-pacientes[113051813].update(dictpact4)
-print(pacientes)
+
+#forma 1 de actualizar- ingresando toda la información
+#    dictpact4={'identificación':113051813,'Nombre':'Margarita','Apellido':'Baudrit','Tel':8526,'Dirección':'San Jose','listaenfermedades':['diabetes','migrañas','embarazo'], 'listamed':['insulina','botox']}
+#    pacientes[113051813].update(dictpact4)
+#    print(pacientes)
+
+#forma 2 de actualizar- agregando 1 condicion mas.
+dictpact5='embarazo'
+pacientes[113051813]['listaenfermedades'].append(dictpact5)
+print("Esta es la lista de enfermedades de 1 paciente en particular actualizada:",pacientes)
 
 #Agregar más medicamentos en un paciente en particular
-dictpact2={'identificación':112320312,'Nombre':'Monica','Apellido':'Penon','Tel':7896,'Dirección':'San Francsico','listaenfermedades':['dolor de estomago'], 'listamed':['jarabe','clorotrimeton','alergodex']}
-pacientes[112320312].update(dictpact2)
-print(pacientes)
+#forma 1 de actualizar- ingresando toda la información
+#    dictpact2={'identificación':112320312,'Nombre':'Monica','Apellido':'Penon','Tel':7896,'Dirección':'San Francsico','listaenfermedades':['dolor de estomago'], 'listamed':['jarabe','clorotrimeton','alergodex']}
+#    pacientes[112320312].update(dictpact2)
+#    print(pacientes)
+
+#Forma 2 de actualizar-agregando 1 medicamente mas a la lista
+dictpact2='clorotrimeton'
+pacientes[112320312]['listamed'].append(dictpact2)
+print("Esta es la lista de medicamentos de 1 paciente en particular actualizada:",pacientes)
 
 #Generar reporte de las enfermedades tratadas en la clínica
 p=[]
 for person in pacientes:
     p= p+pacientes[person]['listaenfermedades']
     mylist=list(dict.fromkeys(p))
-print(mylist)
+print("Esta es la lista de enfermedades:",mylist)
 
 #Generar reporte de los medicamentos entregados en la clínica
 m=[]
 for person in pacientes:
     m= m+pacientes[person]['listamed']
     mylist=list(dict.fromkeys(m))
-print(mylist)
+print("Esta es la lista de medicamentos entregados:",mylist)
 
 #Comparar 2 pacientes en particular: cuales enfermedades tienen en común. Cuales no?. Lo mismo con los medicamentos.
 
 #comparar enfermedades
 a = set(pacientes[112320312]['listaenfermedades'])
 b = set(pacientes[113051813]['listaenfermedades'])
-
+print("Esta es la comparación de enfermedades:")
 # Letras en a pero no en b
 print('en paciente 1 pero no en 2',a - b)
 # letras en a o en b o en ambos
@@ -93,7 +106,7 @@ print('en paciente 1 o en paciente 2 pero no en ambos', a ^ b)
 #comparar medicamentos
 a = set(pacientes[112320312]['listamed'])
 b = set(pacientes[113051813]['listamed'])
-
+print("Esta es la comparación de medicamentos:")
 # Letras en a pero no en b
 print('en paciente 1 pero no en 2',a - b)
 # letras en a o en b o en ambos
